c132bc8392efa4b298168ed1f66149e4cf7f3d9a
Alvarad0 <alvarado.programador@gmail.com>
2017-03-03 16:50:38 -0600

    Integracion de la Galeria, Terapias y Servicios

--- js/galeria.js
+++ js/galeria.js
@@ -1,36 +1,15 @@
-$( ".galeria img" ).click(function() {
-   $("#btn-anterior").show();
-   $("#btn-siguiente").show();
-  imagen = $(this).attr( "src");
-  $( ".modal-body" ).html("<img src="+imagen+">");
-  if ($('.galeria').find('img:first').attr( "src") == imagen) {
-    $("#btn-anterior").hide();
-  } 
-  if ($('.galeria').find('img:last').attr( "src") == imagen) {
-    $("#btn-siguiente").hide();
-  }  
-  $('#myModal').modal('show'); 
-});
+$(document).ready(function() {
+  $('.popup-btn').click(function(e) {
+    $('.popup-wrap').fadeIn(250);
+    $('.popup-box').removeClass('transform-out').addClass('transform-in');
 
+    e.preventDefault();
+  });
 
-$("#btn-siguiente").click(function() {
-  imagen = $("img[src$='"+imagen+"']").next().attr("src");
-  $( ".modal-body" ).html("<img src="+imagen+">"); 
-  if ($('.galeria').find('img:last').attr( "src") == imagen) {
-    $("#btn-siguiente").hide();
-  }
-  if ($('.galeria').find('img:first').attr( "src") != imagen) {
-    $("#btn-anterior").show();
-  }
-});
+  $('.popup-close').click(function(e) {
+    $('.popup-wrap').fadeOut(500);
+    $('.popup-box').removeClass('transform-in').addClass('transform-out');
 
-$("#btn-anterior").click(function() {
-  imagen = $("img[src$='"+imagen+"']").prev().attr("src");
-  $( ".modal-body" ).html("<img src="+imagen+">");
-  if ($('.galeria').find('img:first').attr( "src") == imagen) {
-    $("#btn-anterior").hide();
-  }
-  if ($('.galeria').find('img:last').attr( "src") != imagen) {
-    $("#btn-siguiente").show();
-  }
+    e.preventDefault();
+  });
 });
--- js/script.js
+++ js/script.js
@@ -15,3 +15,34 @@ $(document).ready(function ($) {
         
         
 });
+
+//Teparias
+$(function(){
+  $(".accordion-titulo").click(function(e){
+           
+        e.preventDefault();
+    
+        var contenido=$(this).next(".accordion-content");
+
+        if(contenido.css("display")=="none"){ //open		
+          contenido.slideDown(250);			
+          $(this).addClass("open");
+        }
+        else{ //close		
+          contenido.slideUp(250);
+          $(this).removeClass("open");	
+        }
+
+      });
+});
+
+//Galeria
+$(document).ready(function() {
+$('.thumbnail').click(function(){
+      $('.modal-body').empty();
+  	var title = $(this).parent('a').attr("title");
+  	$('.modal-title').html(title);
+  	$($(this).parents('div').html()).appendTo('.modal-body');
+  	$('#myModal').modal({show:true});
+});
+});
